337355036ae6ce91d36337fe5c440c12ed530667
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-03-26 14:24:28 +0500

    Corrected secret api keys

--- NewUpstorm.Data/Configurations/AppAPISetting.cs
+++ NewUpstorm.Data/Configurations/AppAPISetting.cs
@@ -8,9 +8,9 @@ namespace NewUpstorm.Data.Configurations
     public class AppAPISetting
     {
         public const string PATH = 
-        $"https://api.openweathermap.org/data/2.5/weather?q=Tashkent&appid=c45d8b9a884e5ff945eb105a164c78e7&units=metric";
+        $"https://api.openweathermap.org/data/2.5/weather?q=Tashkent&appid=[EnterYourAPIKey]units=metric";
 
         public const string WEEKLY_PATH =
-           $"https://api.openweathermap.org/data/2.5/forecast?q=Tashkent,uz&appid=c45d8b9a884e5ff945eb105a164c78e7";
+           $"https://api.openweathermap.org/data/2.5/forecast?q=Tashkent,uz&appid=[EnterYourAPIKey]";
     }
 }
--- NewUpstorm.Presentation/Program.cs
+++ NewUpstorm.Presentation/Program.cs
@@ -12,49 +12,6 @@ internal class Program
     static async Task Main(string[] args)
     {
 
-        //IForecastRepository repo = new ForecastRepository();
-
-        //var fore = await repo.SelectCurrentForecastAsync("London");
-
-        //string path = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q=Tashkent&appid=c45d8b9a884e5ff945eb105a164c78e7";
-
-        //HttpClient client = new HttpClient();
-        //var response = (await client.GetAsync(path));
-        //string content = await response.Content.ReadAsStringAsync();
-
-        //var data = JsonConvert.DeserializeObject<IEnumerable<RootObject>>(content);
-
-
-        // Replace YOUR_API_KEY with your actual API key
-        string apiKey = "YOUR_API_KEY";
-        string url = $"https://api.openweathermap.org/data/2.5/forecast?q=Tashkent,uz&appid=c45d8b9a884e5ff945eb105a164c78e7";
-
-        using (var httpClient = new HttpClient())
-        {
-            var response = httpClient.GetAsync(url).Result;
-            var content = response.Content.ReadAsStringAsync().Result;
-
-            // Parse the response JSON
-            var data = JObject.Parse(content);
-
-            // Extract the list of weather forecasts
-            var forecasts = data["list"];
-
-            //IEnumerable<RootObject> res = JsonConvert.DeserializeObject<IEnumerable<RootObject>>(forecasts);
-
-            // Loop through the forecasts and display the data
-            foreach (var forecast in forecasts)
-            {
-                var date = forecast["dt_txt"];
-                var temp = forecast["main"]["temp"];
-                var humidity = forecast["main"]["humidity"];
-                var description = forecast["weather"][0]["description"];
-
-                Console.WriteLine($"Date: {date}\nTemperature: {temp}\nHumidity: {humidity}\nDescription: {description}\n");
-            }
-        }
-        
-
-
+       
     }
 }

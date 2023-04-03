84f1c19218d913b4e824fc27c4a4949bed623b34
Firdavs Muzaffarov <117918038+Muzaffarov-Firdavs@users.noreply.github.com>
2023-04-03 20:31:57 +0500

    Update AppAPISetting.cs
    added api word

--- NewUpstorm.Data/Configurations/AppAPISetting.cs
+++ NewUpstorm.Data/Configurations/AppAPISetting.cs
@@ -8,7 +8,7 @@ namespace NewUpstorm.Data.Configurations
     public class AppAPISetting
     {
         public const string PATH = 
-        $"https://api.openweathermap.org/data/2.5/weather?q=Tashkent&units=metric&appid=[Enter your api]";
+        $"https://api.openweathermap.org/data/2.5/weather?q=Tashkent&units=metric&appid=[Enter your api key]";
 
         public const string WEEKLY_PATH =
            $"https://api.openweathermap.org/data/2.5/forecast?q=Tashkent,uz&appid=[EnterYourAPIKey]";

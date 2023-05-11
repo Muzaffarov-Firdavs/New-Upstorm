98992bd97058890b07fda926d856326c14df3be2
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-05-11 12:37:38 +0500

    added user api rest

--- NewUpstorm.Web/Controllers/UsersController.cs
+++ NewUpstorm.Web/Controllers/UsersController.cs
@@ -1,7 +1,35 @@
-﻿namespace NewUpstorm.Web.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using NewUpstorm.Service.DTOs;
+using NewUpstorm.Service.Interfaces;
+
+namespace NewUpstorm.Web.Controllers
 {
     public class UsersController : BaseController
     {
+        private readonly IUserService userService;
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
 
+        [HttpPost("create")]
+        public async Task<IActionResult> PostAsync(UserCreationDto dto)
+            => Ok(await this.userService.AddAsync(dto));
+
+        [HttpPut("post")]
+        public async Task<IActionResult> PutAsync(UserForUpdateDto dto)
+            => Ok(await this.userService.ModifyAsync(dto));
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteAsync(long id)
+            => Ok(await this.userService.RemoveAsync(id));
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetAsync(long id)
+            => Ok(await this.userService.RetriewByIdAsync(id));
+
+        [HttpGet("list")]
+        public async Task<IActionResult> GetAsync(string search = null)
+            => Ok(await this.userService.RetriewAllAsync(search));
     }
 }
---
+++ NewUpstorm.Web/Extensions/ServiceExtentions.cs
@@ -0,0 +1,18 @@
+﻿using NewUpstorm.Data.IRepositories;
+using NewUpstorm.Data.Repositories;
+using NewUpstorm.Service.Interfaces;
+using NewUpstorm.Service.Services;
+
+namespace NewUpstorm.Web.Extensions
+{
+    public static class ServiceExtentions
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IForecastService, ForecastService>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}

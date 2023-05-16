0c4ba7c383b0292b7cfb3bb74781748f22ab171a
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-05-16 17:25:55 +0500

    changes euneedable properties

--- NewUpstorm.Web/Program.cs
+++ NewUpstorm.Web/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

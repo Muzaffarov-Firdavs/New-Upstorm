7280f5bdac01106e15389571fdac89ffa4bd3753
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-05-21 17:30:32 +0500

    added email service api

---
+++ NewUpstorm.Web/Controllers/EmailsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewUpstorm.Service.Interfaces;
+
+namespace NewUpstorm.Web.Controllers
+{
+    public class EmailsController : BaseController
+    {
+        private readonly IEmailService emailService;
+        public EmailsController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SendVerifcationAsync(string email)
+            => Ok(new
+            {
+                Code = 200,
+                Message = "Success",
+                Data = await this.emailService.SendEmailAsync(email)
+            });
+    }
+}

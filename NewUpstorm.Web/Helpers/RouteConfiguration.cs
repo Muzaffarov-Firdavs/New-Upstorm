67617cabf54bbc84ddb4df71f64c46b9a5b53c66
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-05-21 19:11:11 +0500

    added route configuration

---
+++ NewUpstorm.Web/Helpers/RouteConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NewUpstorm.Web.Helpers
+{
+    public class RouteConfiguration
+    {
+        public string TransformOutbound(object value)
+        {
+            // Slugify value
+            return value == null ? null : Regex.Replace(value.ToString(), "([a-z])([A-Z])", "$1-$2").ToLower();
+        }
+    }
+}
